21747f73d999edd0921b6348bf2f826705151c26
Jacob Russell <Jake.Russell@balladhealth.org>
2022-03-19 21:38:43 -0400

    adding inventorycount model

--- VacTrac_0.2/Controllers/HomeController.cs
+++ VacTrac_0.2/Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using VacTrac_0._2.Models;
+using VacTrac.Models;
 
-namespace VacTrac_0._2.Controllers
+namespace VacTrac.Controllers
 {
     public class HomeController : Controller
     {
--- VacTrac_0.2/Models/InventoryCount.cs
+++ VacTrac_0.2/Models/InventoryCount.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 
 namespace VacTrac.Models
 {
-    public class Vaccine
+    public class InventoryCount
     {
         public int ID { get; set; }
-        public string VaccineName { get; set; }
-        public string Description { get; set; }
+        public string VaccineID { get; set; }
+        public bool IsPrivate { get; set; }
+        public int Count { get; set; }
     }
 }
