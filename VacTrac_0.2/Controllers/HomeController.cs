cba28dda6a2e023914ee84a9730ebfdb1e8e30c2
Jacob Russell <Jake.Russell@balladhealth.org>
2022-03-24 09:54:52 -0400

    Adding manipulator pages for vaccine inputs

--- VacTrac_0.2/Controllers/HomeController.cs
+++ VacTrac_0.2/Controllers/HomeController.cs
@@ -34,6 +34,76 @@ namespace VacTrac.Controllers
             return View();
         }
 
+        public IActionResult CreateVaccine()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateVaccine(Vaccines vaccine)
+        {
+            _myDbContext.Vaccines.Add(vaccine);
+            _myDbContext.SaveChanges();
+            return RedirectToAction("Vaccines");
+        }
+
+
+        public IActionResult EditVaccine(int? id)
+        {
+            var vaccine = (from v in _myDbContext.Vaccines
+                           where v.ID == id
+                           select v).FirstOrDefault();
+
+            return View(vaccine);
+        }
+        [HttpPost]
+        public IActionResult EditVaccine(Vaccines vaccine)
+        {
+            var vac = (from v in _myDbContext.Vaccines
+                           where v.ID == vaccine.ID
+                           select v).FirstOrDefault();
+
+            vac.Description = vaccine.Description;
+            vac.InventoryTotal = vaccine.InventoryTotal;
+            vac.Private = vaccine.Private;
+            vac.VaccineName = vaccine.VaccineName;
+            vac.WeeklyPAR = vaccine.WeeklyPAR;
+
+            _myDbContext.SaveChanges();
+
+            return RedirectToAction("Vaccines");
+        }
+
+
+        public IActionResult VaccineDetails(int? id)
+        {
+            var vaccine = (from v in _myDbContext.Vaccines
+                           where v.ID == id
+                           select v).FirstOrDefault();
+
+            return View(vaccine);
+        }
+
+
+        public IActionResult DeleteVaccine(int? id)
+        {
+            var vaccine = (from v in _myDbContext.Vaccines
+                           where v.ID == id
+                           select v).FirstOrDefault();
+
+            return View(vaccine);
+        }
+        [HttpPost]
+        public IActionResult DeleteVaccine(Vaccines vaccine)
+        {
+            var vac = (from v in _myDbContext.Vaccines
+                       where v.ID == vaccine.ID
+                       select v).FirstOrDefault();
+
+            _myDbContext.Remove(vac);
+            _myDbContext.SaveChanges();
+
+            return RedirectToAction("Vaccines");
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
